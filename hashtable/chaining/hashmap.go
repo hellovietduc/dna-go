c5f5cfda6e46261c68008810e8aad9f27f04e256
Nguyễn Việt Đức <vietduc01100001@gmail.com>
2020-06-14 16:13:50 +0700

    Add Hash Table implementation using chaining with Linked Lists (#11)

---
+++ hashtable/chaining/hashmap.go
@@ -0,0 +1,154 @@
+package chaining
+
+const (
+	// KeyNotFound is a constant indicates that
+	// a key cannot be found in the HashMap.
+	KeyNotFound      = 0
+	defaultCapacity  = 8
+	upperLoadFactor  = 0.75
+	lowerLoadFactor  = 0.2
+	growShrinkFactor = 2
+)
+
+type node struct {
+	key   int
+	value int
+	next  *node
+	prev  *node
+}
+
+// HashMap is a struct for storing key-value data.
+type HashMap struct {
+	items []*node
+	size  int
+}
+
+// NewHashMap creates a new HashMap instance.
+func NewHashMap() *HashMap {
+	return &HashMap{
+		items: make([]*node, defaultCapacity),
+	}
+}
+
+// Insert adds a new pair of key-value to the HashMap.
+// If the key already exists, it overwrites the key's value.
+func (h *HashMap) Insert(key int, value int) {
+	if h.getLoadFactor() >= upperLoadFactor {
+		h.grow()
+	}
+
+	newNode := &node{
+		key:   key,
+		value: value,
+	}
+
+	foundNode, index := h.searchNode(key)
+	if foundNode != nil {
+		// overwrite value of this key
+		foundNode.value = value
+		return
+	}
+
+	h.size++
+	if curNode := h.items[index]; curNode == nil {
+		// first node in the linked list
+		h.items[index] = newNode
+	} else {
+		// last node in the linked list
+		for curNode.next != nil {
+			curNode = curNode.next
+		}
+		curNode.next = newNode
+		newNode.prev = curNode
+	}
+}
+
+// Delete removes a key from the HashMap.
+func (h *HashMap) Delete(key int) {
+	foundNode, index := h.searchNode(key)
+	if foundNode == nil {
+		return
+	}
+
+	if h.getLoadFactor() < lowerLoadFactor {
+		h.shrink()
+	}
+
+	h.size--
+
+	if foundNode.prev == nil {
+		// first node in the linked list
+		h.items[index] = foundNode.next
+		return
+	}
+
+	if foundNode.next == nil {
+		// last node in the linked list
+		foundNode.prev.next = nil
+		return
+	}
+
+	prevNode := foundNode.prev
+	nextNode := foundNode.next
+	prevNode.next = nextNode
+	nextNode.prev = prevNode
+}
+
+// Search returns the value for the given key.
+// If the key cannot be found, it returns KeyNotFound.
+func (h *HashMap) Search(key int) int {
+	if foundNode, _ := h.searchNode(key); foundNode != nil {
+		return foundNode.value
+	}
+	return KeyNotFound
+}
+
+func (h *HashMap) searchNode(key int) (*node, int) {
+	index := h.hash(key)
+	curNode := h.items[index]
+
+	for curNode != nil {
+		if curNode.key == key {
+			return curNode, index
+		}
+
+		curNode = curNode.next
+	}
+
+	return nil, index
+}
+
+func (h *HashMap) capacity() int {
+	return len(h.items)
+}
+
+func (h *HashMap) getLoadFactor() float64 {
+	return float64(h.size) / float64(len(h.items))
+}
+
+func (h *HashMap) hash(key int) int {
+	return key % len(h.items)
+}
+
+func (h *HashMap) grow() {
+	newLength := len(h.items) * growShrinkFactor
+	h.rehash(newLength)
+}
+
+func (h *HashMap) shrink() {
+	newLength := len(h.items) / growShrinkFactor
+	h.rehash(newLength)
+}
+
+func (h *HashMap) rehash(newLength int) {
+	oldArr := h.items
+	h.items = make([]*node, newLength)
+	h.size = 0
+
+	for _, curNode := range oldArr {
+		for curNode != nil {
+			h.Insert(curNode.key, curNode.value)
+			curNode = curNode.next
+		}
+	}
+}
---
+++ hashtable/chaining/hashmap_test.go
@@ -0,0 +1,69 @@
+package chaining
+
+import (
+	"testing"
+)
+
+func TestHashMap(t *testing.T) {
+	hashMap := NewHashMap()
+
+	t.Run("Insert new keys, get correct values for inserted keys", func(t *testing.T) {
+		for i := 1; i < 4; i++ {
+			hashMap.Insert(i, i)
+
+			if value := hashMap.Search(i); value != i {
+				t.Errorf("Wrong value for inserted key %d, expected %d, found %d", i, i, value)
+			}
+		}
+	})
+
+	t.Run("Overwrite existed keys, get correct values for overwritten keys", func(t *testing.T) {
+		for i := 1; i < 4; i++ {
+			hashMap.Insert(i, i*10)
+
+			if value := hashMap.Search(i); value != i*10 {
+				t.Errorf("Wrong value for overwritten key %d, expected %d, found %d", i, i*10, value)
+			}
+		}
+	})
+
+	t.Run("Insert more keys so the HashMap must grow bigger in capacity", func(t *testing.T) {
+		capacityBefore := hashMap.capacity()
+		for i := 4; i < 20; i++ {
+			hashMap.Insert(i, i)
+		}
+		capacityAfter := hashMap.capacity()
+		if capacityAfter < capacityBefore {
+			t.Errorf("Capacity should grow when inserting more keys, before %d, after %d", capacityBefore, capacityAfter)
+		}
+	})
+
+	t.Run("Search for non-existed keys, get KeyNotFound", func(t *testing.T) {
+		for i := 30; i > 34; i-- {
+			if value := hashMap.Search(i); value != KeyNotFound {
+				t.Errorf("Search for non-existed key %d should return KeyNotFound, found %d", i, value)
+			}
+		}
+	})
+
+	t.Run("Delete existed keys, get KeyNotFound for deleted keys", func(t *testing.T) {
+		for i := 1; i < 4; i++ {
+			hashMap.Delete(i)
+
+			if value := hashMap.Search(i); value != KeyNotFound {
+				t.Errorf("Search for deleted key %d should return KeyNotFound, found %d", i, value)
+			}
+		}
+	})
+
+	t.Run("Delete more keys so the HashMap must reduce smaller in capacity", func(t *testing.T) {
+		capacityBefore := hashMap.capacity()
+		for i := 4; i < 20; i++ {
+			hashMap.Delete(i)
+		}
+		capacityAfter := hashMap.capacity()
+		if capacityAfter >= capacityBefore {
+			t.Errorf("Capacity should shrink when deleting more keys, before %d, after %d", capacityBefore, capacityAfter)
+		}
+	})
+}
